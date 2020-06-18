a17eb342f8e4609de92daf33fe0394b89f873622
ADRIJA BHATTACHARJEE <65231508+momo200716@users.noreply.github.com>
2020-06-18 19:49:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var  car,wall;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  weight=createSprite(1500, 200, 60, height/2);
+  car=createSprite(50,200,50,50)
+  speed = random(55,90)
+  weight = random(400,1500)
+  car.velocityX= speed;
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
